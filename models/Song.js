8a824b9307d593c2345675edc291baaa0ca364e3
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-08-29 14:15:00 +0100

    Update songschema with a method to toggle likes

---
+++ models/Song.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const SongSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  artist: {
+    type: String,
+    required: true,
+  },
+  artistId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Artiste",
+    required: true,
+  },
+  coverImage: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/ojigs/image/upload/v1686067806/images_-_2022-09-26T080853.354_it1tgl.jpg",
+  },
+  album: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Album",
+  },
+  duration: String,
+  audioURL: String,
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  releaseDate: Date,
+  genre: String,
+});
+
+SongSchema.methods.toogleLike = async function (userId) {
+  const isLiked = this.likes.includes(userId);
+  if (isLiked) {
+    this.likes.pull(userId);
+  } else {
+    this.likes.addToSet(userId);
+  }
+  await this.save();
+  return !isLiked;
+};
+
+module.exports = mongoose.model("Song", SongSchema);
