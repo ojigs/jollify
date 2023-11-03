7417e9e476a03a5f76296eb610440a434ea72fdd
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-11-03 16:40:04 +0100

    Prep server for vercel deployment

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("MERN music app");
+  res.send("Welcome to Jollify");
 });
 
 app.use("/auth", authRoutes);
