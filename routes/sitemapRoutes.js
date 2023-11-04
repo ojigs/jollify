10a518c026df6faa9af8949bf2054ecb30f52024
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-11-04 13:32:04 +0100

    Reconfig project server directory

--- routes/sitemapRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getSitemap } = require("../controllers/sitemapController");
-
-router.get("/", getSitemap);
-
-module.exports = router;
