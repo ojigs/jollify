e5a3d38e59b271f32c679f2c6b9d09f03909f337
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-11-06 22:29:38 +0100

    Allow users view their password on signup

--- server/index.js
+++ server/index.js
@@ -19,7 +19,12 @@ connectDB();
 
 require("./config/passport")(passport);
 
-app.use(cors({ origin: "https://jollify.vercel.app", credentials: true }));
+app.use(
+  cors({
+    origin: ["https://jollify.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
